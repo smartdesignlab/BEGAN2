ee67649cd224964b7ae35c71c6d250eb05145276
Smart Design Lab <41012084+smartdesignlab@users.noreply.github.com>
2019-09-19 12:18:44 +0900

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-curr_date=`date +%y-%m-%d-%H-%M`
-log_dir=./logs
-mkdir -p ${log_dir}
-export CUDA_VISIBLE_DEVICES=0,1,2,3
-nohup python3 main.py --n_z=128 --n_batch=16 --data_dir=/home/ubuntu/app1/input --dataset=wheeldesign --n_epoch=40000 --n_save_img_step=100 --n_save_log_step=30 --log_dir=${log_dir} > ${log_dir}/${curr_date}_log.txt &
-
-sleep 1
-tail -100f ${log_dir}/${curr_date}_log.txt
